4db72f80805ed0040ccf777a3bb3ebdc62cdda25
Terje Sandstrom <terje.sandstrom.personal@outlook.com>
2019-11-19 18:08:49 +0100

    updated naming

---
+++ UnitTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace example2
+{
+    public class Tests
+    {
+        
+
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true);
+        }
+    }
+}
